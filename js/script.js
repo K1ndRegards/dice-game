f856293b989da7f314d85dfe9c86213c5f882ec0
K1ndRegards <kindreg4rds@gmail.com>
2025-06-06 18:11:18 +0300

    minor code refactoring

--- js/script.js
+++ js/script.js
@@ -13,21 +13,20 @@ const player1Card = document.querySelector('div.player-1');
 // Player 2
 const player2Card = document.querySelector('div.player-2');
 
-let diceValue;
-let player1TotalScore = 0,
-  player1CurrentScore = 0;
-let player2TotalScore = 0,
-  player2CurrentScore = 0;
+let player1TotalScore = 0;
+let player2TotalScore = 0;
+
+let currentScore = 0;
 
 let player1Turn = true;
 let gameEnded = false;
 
 function updatePlayer1CurrentScore() {
-  document.querySelector('.p1-current-score').innerText = player1CurrentScore;
+  document.querySelector('.p1-current-score').innerText = currentScore;
 }
 
 function updatePlayer2CurrentScore() {
-  document.querySelector('.p2-current-score').innerText = player2CurrentScore;
+  document.querySelector('.p2-current-score').innerText = currentScore;
 }
 
 function updatePlayer1TotalScore() {
@@ -50,14 +49,14 @@ function switchPlayer() {
   }
 }
 
-function rollADice() {
+rollDiceBtn.addEventListener('click', function () {
   if (gameEnded) return;
 
   if (diceImg.classList.contains('hidden')) {
     diceImg.classList.remove('hidden');
   }
   // rolling a dice
-  diceValue = Math.trunc(Math.random() * 6) + 1;
+  const diceValue = Math.trunc(Math.random() * 6) + 1;
 
   // changing the image of the dice
   diceImg.src = `assets/dice-${diceValue}.png`;
@@ -66,10 +65,10 @@ function rollADice() {
   if (diceValue === 1) {
     // removing all the score from current score and changing the player
     if (player1Turn) {
-      player1CurrentScore = 0;
+      currentScore = 0;
       updatePlayer1CurrentScore();
     } else {
-      player2CurrentScore = 0;
+      currentScore = 0;
       updatePlayer2CurrentScore();
     }
 
@@ -79,24 +78,22 @@ function rollADice() {
 
   // if it's player 1 turn
   if (player1Turn) {
-    player1CurrentScore += diceValue;
+    currentScore += diceValue;
     updatePlayer1CurrentScore();
 
     // else it is player 2 turn
   } else {
-    player2CurrentScore += diceValue;
+    currentScore += diceValue;
     updatePlayer2CurrentScore();
   }
-}
-
-rollDiceBtn.addEventListener('click', rollADice);
+});
 
 holdBtn.addEventListener('click', function () {
   if (gameEnded) return;
 
   if (player1Turn) {
-    player1TotalScore += player1CurrentScore;
-    player1CurrentScore = 0;
+    player1TotalScore += currentScore;
+    currentScore = 0;
     updatePlayer1CurrentScore();
     updatePlayer1TotalScore();
 
@@ -108,8 +105,8 @@ holdBtn.addEventListener('click', function () {
 
     switchPlayer();
   } else {
-    player2TotalScore += player2CurrentScore;
-    player2CurrentScore = 0;
+    player2TotalScore += currentScore;
+    currentScore = 0;
     updatePlayer2CurrentScore();
     updatePlayer2TotalScore();
 
@@ -138,11 +135,12 @@ newGameBtn.addEventListener('click', function () {
     player2Card.classList.remove('win-bg');
   }
 
-  // Resetting players scores and updating UI
-  (player1TotalScore = 0), (player1CurrentScore = 0);
+  // Resetting player scores and updating UI
+  currentScore = 0;
+  player1TotalScore = 0;
+  player2TotalScore = 0;
   updatePlayer1CurrentScore();
   updatePlayer1TotalScore();
-  (player2TotalScore = 0), (player2CurrentScore = 0);
   updatePlayer2CurrentScore();
   updatePlayer2TotalScore();
 
